585cc5a92c5e683c0a5327f9488b6d7097145544
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-04-28 22:20:52 +0200

    Replace swup with flamethrower

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-import { defineConfig } from 'astro/config'
-import UnoCSS from 'unocss/astro'
+import { defineConfig } from "astro/config";
+import UnoCSS from "unocss/astro";
 
 export default defineConfig({
-  integrations: [
-    UnoCSS({injectReset:true}),
-  ],
-})
+	integrations: [UnoCSS({ injectReset: true })],
+});
---
+++ src/utils/router.ts
@@ -0,0 +1,5 @@
+import flamethrower from "flamethrower-router";
+
+const router = flamethrower({ prefetch: "visible", pageTransitions: true });
+
+export default router;
