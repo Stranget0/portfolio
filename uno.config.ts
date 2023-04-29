97def9b3a12a3b4b33f1ab313534262591feb8f2
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-04-29 23:18:13 +0200

    Add uno custom theme support

--- uno.config.ts
+++ uno.config.ts
@@ -10,18 +10,17 @@ import {
 } from "unocss";
 
 export default defineConfig({
-	shortcuts: [
-		// ...
-	],
+	shortcuts: {},
 	theme: {
 		colors: {
-			// ...
+			dblue: "#020417",
+			dviolet: "#090416",
 		},
 	},
 	presets: [
 		presetUno(),
 		presetAttributify(),
-		presetIcons(),
+		presetIcons({ autoInstall: true, prefix: "i-", warn: true }),
 		presetTypography(),
 		presetWebFonts({
 			provider: "google",
@@ -32,4 +31,27 @@ export default defineConfig({
 		}),
 	],
 	transformers: [transformerDirectives(), transformerVariantGroup()],
+	rules: [
+		[
+			/theme-default/,
+			(_, { theme }) => {
+				const style: {
+					[k in `--${"primary" | "secondary" | "tertiary"}-${number}`]: string;
+				} = {};
+				for (let i = 50; i <= 1000; i += i >= 100 && i < 900 ? 100 : 50) {
+					style[`--primary-${i}`] = theme.colors.blue[i];
+					style[`--secondary-${i}`] = theme.colors.fuchsia[i];
+					style[`--tertiary-${i}`] = theme.colors.teal[i];
+				}
+				return style;
+			},
+		],
+		[
+			/(?:primary|secondary|tertiary)-(?:50|100|200|300|400|500|600|700|800|900|950|1000)/,
+			([m]) => ({
+				color: `var(--${m})`,
+				transition: ".25s color ease-out",
+			}),
+		],
+	],
 });
