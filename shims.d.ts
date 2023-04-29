cd1cd32174dad3c6d1ab62bb6139810177433ba5
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-04-29 23:22:30 +0200

    fix prose ts error

--- shims.d.ts
+++ shims.d.ts
@@ -2,6 +2,8 @@ import type { AttributifyAttributes } from "@unocss/preset-attributify";
 
 declare global {
 	namespace astroHTML.JSX {
-		interface HTMLAttributes extends AttributifyAttributes {}
+		interface HTMLAttributes
+			extends AttributifyAttributes,
+				Partial<Record<"prose", string | boolean>> {}
 	}
 }
