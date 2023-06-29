b2670938b7e95b82d37aa99bb8132fa700e5bf11
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-06-29 20:40:37 +0200

    Remove shims to make TS faster

--- shims.d.ts
+++ shims.d.ts
@@ -1,9 +1,10 @@
-import type { AttributifyAttributes } from "@unocss/preset-attributify";
+// BUG this slows down typescript too much
+// import type { AttributifyAttributes } from "@unocss/preset-attributify";
 
-declare global {
-	namespace astroHTML.JSX {
-		interface HTMLAttributes
-			extends AttributifyAttributes,
-				Partial<Record<"prose" | "snap", string | boolean>> {}
-	}
-}
+// declare global {
+// 	namespace astroHTML.JSX {
+// 		interface HTMLAttributes
+// 			extends AttributifyAttributes,
+// 				Partial<Record<"prose" | "snap", string | boolean>> {}
+// 	}
+// }
