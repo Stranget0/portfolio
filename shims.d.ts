789c7b687dbac004a3b581af6ba5de4962d205c8
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-07-17 16:58:37 +0200

    Add netlify attr to forms

--- shims.d.ts
+++ shims.d.ts
@@ -1,10 +1,10 @@
 // BUG this slows down typescript too much
 // import type { AttributifyAttributes } from "@unocss/preset-attributify";
-
-// declare global {
-// 	namespace astroHTML.JSX {
+	declare namespace astroHTML.JSX {
+		interface FormHTMLAttributes{
+			netlify?: boolean
+		}
 // 		interface HTMLAttributes
 // 			extends AttributifyAttributes,
 // 				Partial<Record<"prose" | "snap", string | boolean>> {}
-// 	}
-// }
+	}
