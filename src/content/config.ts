61001724cc7f3e8f5a6cefb4132985db075dbab1
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-07-21 19:06:52 +0200

    Add about-me audio data and transcriptions schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,14 @@
 import { defineCollection } from "astro:content";
-import { linkSchema, projectSchema, technologySchema } from "../schemas";
+import {
+	linkSchema,
+	projectSchema,
+	technologySchema,
+	transcriptSchema,
+} from "../schemas";
 
 export const collections = {
 	technologies: defineCollection({ type: "content", schema: technologySchema }),
 	projects: defineCollection({ type: "content", schema: projectSchema }),
 	links: defineCollection({ type: "content", schema: linkSchema }),
+	transcripts: defineCollection({ type: "data", schema: transcriptSchema }),
 };
