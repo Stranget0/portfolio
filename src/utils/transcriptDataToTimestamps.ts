cbf092344413d2b3f19d3981e40468416afad813
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-07-25 01:44:44 +0200

    Refactor transcript transformer function

--- src/utils/transcriptDataToTimestamps.ts
+++ src/utils/transcriptDataToTimestamps.ts
@@ -1,9 +1,21 @@
 import type { Transcripts } from "src/schemas";
+import { lerp } from "three/src/math/MathUtils";
 
 export default function transcriptDataToTimestamps(data: Transcripts) {
-	return data.results.flatMap(({ alternatives }) =>
+	const dataToMap = data.results.flatMap(({ alternatives }) =>
 		alternatives.flatMap(({ words }) =>
-			words.map(({ word, endTime }) => ({ word, timestamp: endTime }))
+			words.map(({ word, startTime, endTime }) => ({
+				word,
+				timestamp: lerp(strStampToMs(startTime), strStampToMs(endTime), 0.5),
+			}))
 		)
 	);
+
+	const timestamps = dataToMap.map((entry) => entry.timestamp);
+	const words = dataToMap.map((entry) => entry.word);
+	return { timestamps, words };
+}
+
+function strStampToMs(strStamp: string) {
+	return parseFloat(strStamp) * 1000;
 }
