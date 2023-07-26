6de508f26bd7d7f98a1a85a92e949177d7e18207
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-07-26 14:09:21 +0200

    Split fox animations to individual files

--- src/utils/animation.ts
+++ src/utils/animation.ts
@@ -1,21 +1,42 @@
-import type { AnimationAction, AnimationMixer } from "three";
+import {
+	LoopRepeat,
+	type AnimationAction,
+	type AnimationClip,
+	type AnimationMixer,
+} from "three";
 import randomIntBetween from "./randomIntBetween";
 import addSingleEventListener from "./singleEvent";
+import { loadDracoGLTF } from "./loadDracoGLTF";
 
-export function handleActionPlay(
-	action: AnimationAction,
+export type ActionLoader = {
+	action: null | AnimationAction;
+	load: () => Promise<AnimationAction>;
+};
+
+type Animation = AnimationAction | ActionLoader;
+
+export async function handleActionPlay<T extends Animation>(
+	actionOrActionLoader: T,
 	fadeDuration: number,
-	idleActions: AnimationAction[],
-	blockedBy: AnimationAction[] = [action]
+	idleActionsOrLoaders: T[],
+	blockedBy: T[] = [actionOrActionLoader]
 ) {
 	// Abort when user is out of the tab
 	if (document.hidden) return;
 
+	let action: AnimationAction;
+	if ("load" in actionOrActionLoader)
+		action = await actionOrActionLoader.load();
+	else action = actionOrActionLoader;
+
 	let isActionFadeIn = false;
 	let isBlocked = false;
-	for (const blockedAction of blockedBy) {
-		if (blockedAction.isRunning()) {
-			if (idleActions.includes(blockedAction)) isActionFadeIn = true;
+	for (const blockedActionOrLoader of blockedBy) {
+		const blockedAction = getAction(blockedActionOrLoader);
+		if (blockedAction?.isRunning()) {
+			if (idleActionsOrLoaders.includes(blockedActionOrLoader))
+				isActionFadeIn = true;
+
 			if (blockedAction === action) isBlocked = true;
 			blockedAction.fadeOut(fadeDuration);
 
@@ -32,23 +53,28 @@ export function handleActionPlay(
 }
 
 export function createRepeatable(
-	actions: AnimationAction[] | AnimationAction,
+	actionsOrLoaders: Animation[] | Animation,
 	fadeDuration: number,
-	idleActions: AnimationAction[],
-	blockedBy?: AnimationAction[]
+	idleActionsOrLoaders: Animation[],
+	blockedBy?: Animation[]
 ) {
 	let timeoutId = 0;
 	return function play(getRepeatDelay?: () => number, shouldPlay = false) {
 		clearTimeout(timeoutId);
 
 		if (shouldPlay) {
-			let action;
-			if (Array.isArray(actions)) {
-				const index = randomIntBetween(0, actions.length - 1);
-				action = actions[index];
-			} else action = actions;
+			let actionOrLoader;
+			if (Array.isArray(actionsOrLoaders)) {
+				const index = randomIntBetween(0, actionsOrLoaders.length - 1);
+				actionOrLoader = actionsOrLoaders[index];
+			} else actionOrLoader = actionsOrLoaders;
 
-			handleActionPlay(action, fadeDuration, idleActions, blockedBy);
+			handleActionPlay(
+				actionOrLoader,
+				fadeDuration,
+				idleActionsOrLoaders,
+				blockedBy
+			);
 		}
 		if (getRepeatDelay)
 			timeoutId = window.setTimeout(
@@ -92,4 +118,38 @@ export function addSingleListenerForAction(
 		},
 		hasSameAction
 	);
+}
+
+export function sourceToLoader(
+	mixer: AnimationMixer,
+	onLoad?: (action: AnimationAction) => void
+): (value: string) => ActionLoader {
+	return (source) => {
+		return {
+			action: null,
+			async load() {
+				const { animations } = await loadDracoGLTF(
+					`models/fox/individualAnimations/${source}.glb`
+				);
+				const clip: AnimationClip = animations[0];
+				const clipped = mixer.clipAction(clip);
+				clipped.loop = LoopRepeat;
+				clipped.repetitions = 1;
+				stopAction(clipped);
+				onLoad?.(clipped);
+				this.action = clipped;
+				return clipped;
+			},
+		};
+	};
+}
+
+function getAction(actionOrActionLoader: Animation) {
+	return "action" in actionOrActionLoader
+		? actionOrActionLoader.action
+		: actionOrActionLoader;
+}
+
+export function getActionDuration(a: AnimationAction): number {
+	return a.getClip().duration * 1000;
 }
