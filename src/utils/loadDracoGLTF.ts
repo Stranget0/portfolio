74ec6d9117a279c97ac78d536b6f49b664df0135
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-08-01 12:39:26 +0200

    Make it use only 1 draco loader

--- src/utils/loadDracoGLTF.ts
+++ src/utils/loadDracoGLTF.ts
@@ -2,13 +2,13 @@ import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader";
 import { OnProgress, loadGLTF } from "./loadGLTF";
 
+const draco = new DRACOLoader();
+draco.setDecoderConfig({ type: "js" });
+draco.setDecoderPath("https://www.gstatic.com/draco/v1/decoders/");
+
+const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(draco);
+
 export function loadDracoGLTF(path: string, onProgress?: OnProgress) {
-	const draco = new DRACOLoader();
-	draco.setDecoderConfig({ type: "js" });
-	draco.setDecoderPath("https://www.gstatic.com/draco/v1/decoders/");
-
-	const gltfLoader = new GLTFLoader();
-	gltfLoader.setDRACOLoader(draco);
-
 	return loadGLTF(path, onProgress, gltfLoader);
 }
