c32f74755570702813c59d2e8e8da9e3a6153ad2
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-08-23 21:46:36 +0200

    Add visual regression

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from "cypress";
+import getCompareSnapshotsPlugin from "cypress-visual-regression/dist/plugin";
 
 export default defineConfig({
 	projectId: "vk47ob",
 	experimentalWebKitSupport: true,
-	e2e: { supportFile: false, baseUrl: "http://localhost:3000" },
+	trashAssetsBeforeRuns: true,
+	e2e: {
+		supportFile: false,
+		baseUrl: "http://localhost:3000",
+		setupNodeEvents(on, config) {
+			getCompareSnapshotsPlugin(on, config);
+		},
+		env: {
+			failSilently: false,
+		},
+	},
 });
--- cypress/e2e/index.cy.ts
+++ cypress/e2e/index.cy.ts
@@ -1,6 +1,6 @@
-it('titles are correct', () => {
-  const page = cy.visit('http://localhost:3000');
-	cy.scrollTo(0,50)
-  cy.wait(5000)
-	page.screenshot("fox")
+it("titles are correct", () => {
+	const page = cy.visit("http://localhost:3000");
+	cy.scrollTo(0, 50);
+	cy.wait(5000);
+	cy.compareSnapshot("page", { errorThreshold: 0.1 });
 });
