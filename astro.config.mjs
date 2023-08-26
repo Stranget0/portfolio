33cf1086ebd10659b9133c1069f69552d5f7e975
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-08-26 18:01:12 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,25 @@ import image from "@astrojs/image";
 import UnoCSS from "unocss/astro";
 import { visualizer } from "rollup-plugin-visualizer";
 import mdx from "@astrojs/mdx";
-
 import solidJs from "@astrojs/solid-js";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [UnoCSS(), image({
-		serviceEntryPoint: "@astrojs/image/sharp"
-	}), mdx(), solidJs()],
-	vite: {
-		plugins: [visualizer({
-			template: "sunburst",
-			open: true,
-			gzipSize: true,
-			brotliSize: true,
-			filename: "generated/bundle.html"
-		})]
-	}, 
-	site: "https://msmarzewski.pl", 
-	compressHTML: true,
+  integrations: [UnoCSS(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+		// TODO: Add localization to sitemap
+  }), mdx(), solidJs(), sitemap()],
+  vite: {
+    plugins: [visualizer({
+      template: "sunburst",
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+      filename: "generated/bundle.html"
+    })]
+  },
+  site: "https://msmarzewski.pl",
+  compressHTML: true
 });
