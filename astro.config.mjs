f387cf558394cf13ba201197b8dcdb780a7e9827
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-09-07 11:54:50 +0200

    Add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import { visualizer } from "rollup-plugin-visualizer";
 import mdx from "@astrojs/mdx";
 import solidJs from "@astrojs/solid-js";
 
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [UnoCSS(), mdx(), solidJs()],
-	vite: {
+  integrations: [UnoCSS(), mdx(), solidJs(), partytown({config:{forward:["dataLayer.push"]}})],
+  vite: {
     plugins: [visualizer({
       template: "sunburst",
       gzipSize: true,
