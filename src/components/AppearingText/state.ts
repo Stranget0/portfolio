827422f3e740f50b7f62bf325970386870c96e90
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-09-10 14:28:38 +0200

    Change sever rendered buttons to have loading status

--- src/components/AppearingText/state.ts
+++ src/components/AppearingText/state.ts
@@ -1,5 +1,6 @@
 import { createSignal } from "solid-js";
 import type { States } from "./types";
 
-export const [appearingTextState, setAppearingTextState] =
-	createSignal<States>("default");
+export const [appearingTextState, setAppearingTextState] = createSignal<States>(
+	globalThis.window ? "default" : "loading",
+);
