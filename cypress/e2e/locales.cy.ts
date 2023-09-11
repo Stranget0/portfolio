82a7dd2638b80c00b6e4f5f34f2a0eaa8f9c91d0
Marcin Smarzewski <marcin.smarzewski0@gmail.com>
2023-09-11 02:10:40 +0200

    Fix cypress not detecting tests in locales file

--- cypress/e2e/locales.cy.ts
+++ cypress/e2e/locales.cy.ts
@@ -1,14 +1,19 @@
-import { supportedLanguages } from "@/i18n/constants";
-
 describe("Page locales", () => {
-	supportedLanguages.forEach((lang) => {
-		it(`${lang.toUpperCase()} page has no undefined variables`, async () => {
-			cy.visit(`/${lang}`);
-			cy.get("body").should((body) => {
-				const text = body.text();
-				const reg = /\{.{1,10}\}/;
-				expect(text).not.to.match(reg);
-			});
+	it(`PL page has no undefined variables`, async () => {
+		cy.visit(`/pl`);
+		cy.get("body").should((body) => {
+			const text = body.text();
+			const reg = /\{.{1,10}\}/;
+			expect(text).not.to.match(reg);
+		});
+	});
+
+	it(`EN page has no undefined variables`, async () => {
+		cy.visit(`/en`);
+		cy.get("body").should((body) => {
+			const text = body.text();
+			const reg = /\{.{1,10}\}/;
+			expect(text).not.to.match(reg);
 		});
 	});
 });
